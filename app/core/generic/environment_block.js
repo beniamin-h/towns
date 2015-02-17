29e846532fd032ea6a8191283862430ded20f9a9
benek <benek@benek-EP43T-USB3.(none)>
2015-02-17 01:08:42 +0100

    Gathering resources (initial version)

--- app/core/generic/environment_block.js
+++ app/core/generic/environment_block.js
@@ -14,7 +14,25 @@ angular.module('towns').factory('EnvironmentBlock', ['Environment', function (En
   EnvironmentBlock.prototype.code = '';
 
   EnvironmentBlock.prototype.gatherResources = function (person) {
+    var gathered_resources = {};
+    for (var res_name in this.resources) {
+      if (!this.resources[res_name]) {
+        continue;
+      }
+      var gather_env_ratio = this.resources[res_name] / Environment.getResourceInfo(res_name).max_amount * 10;
+      for (var exploitable_resource_name in Environment.getResourceInfo(res_name).exploitable_resources) {
+        gathered_resources[exploitable_resource_name] = gathered_resources[exploitable_resource_name] || 0;
+        gathered_resources[exploitable_resource_name] += gather_env_ratio *
+          Environment.getResourceInfo(res_name).exploitable_resources[exploitable_resource_name];
+        if (gather_env_ratio <= this.resources[res_name]) {
+          this.resources[res_name] -= gather_env_ratio;
+        } else {
+          this.resources[res_name] = 0;
+        }
+      }
+    }
 
+    return gathered_resources;
   };
 
   EnvironmentBlock.prototype.operate = function () {
